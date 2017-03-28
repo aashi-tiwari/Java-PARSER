523c53908fac1836d033ed7367617751d4df50f0
aashi22 <aashi.tiwari@sjsu.edu>
2017-03-28 12:56:32 -0500

    Create A.java

---
+++ A.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+
+public class A implements E,F{
+ 
+	private int x=10;
+	 
+	private int[] y;
+	 
+	private Collection<B> b;
+	 
+	private C c;
+	 
+	private Collection<D> d;
+	
+    private int sum()
+{
+}
+
+}
